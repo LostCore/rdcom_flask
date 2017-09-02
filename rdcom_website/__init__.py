a3e80e436bd67c1f1f6226bbbc8211189f77275f
LostCore <lostcore@gmail.com>
2017-09-02 17:50:11 +0200

    First breadcrumb

--- rdcom_website/__init__.py
+++ rdcom_website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import helpers
 
 from rdcom_website.blog import get_articles, get_article_content, get_article_file, get_article, parse_article_content
 
@@ -14,6 +15,7 @@ def index():
     articles = get_articles()
     context = {
         'site_title': app.config.get('SITE_TITLE'),
+        'page_type_class': 'home',
         'articles': articles
     }
     return render_template("index.html", **context)
@@ -26,10 +28,16 @@ def blog():
         article_content = get_article_content(article_entry.get('path'))
         article_content = parse_article_content(article_content)
         articles[k].update({'content': article_content})
+    breadcrumb = [
+        ('Home', 'home', helpers.url_for('index'), False),
+        ('Blog', 'blog', helpers.url_for('blog'), True)
+    ]
     context = {
         'site_title': 'Blog - '+app.config.get('SITE_TITLE'),
+        'page_type_class': 'blog',
         'page_title': 'Blog',
-        'articles': articles
+        'articles': articles,
+        'breadcrumb': breadcrumb
     }
     return render_template("blog.html", **context)
 
@@ -40,9 +48,16 @@ def article(post_name):
     article_content = get_article_content(article_file)
     article_content = parse_article_content(article_content)
     the_article = get_article(article_file)
+    breadcrumb = [
+        ('Home', 'home', helpers.url_for('index'), False),
+        ('Blog', 'blog', helpers.url_for('blog'), False),
+        (the_article.get('title'), the_article.get('name'), helpers.url_for('article', post_name=the_article.get('name')), True)
+    ]
     context = {
+        'breadcrumb': breadcrumb,
         'site_title': the_article.get('title')+' - '+app.config.get('SITE_TITLE'),
         'page_title': the_article.get('Blog'),
+        'page_type_class': 'blog-entry',
         'article': the_article,
         'article_content': article_content
     }
